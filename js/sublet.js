016e13bc5bbecf915bfff156fcd70811f6178bcd
fredteumer <frederick.teumer@tufts.edu>
2013-02-10 21:00:31 -0500

    added new subdirectories, began work on jscript

---
+++ js/sublet.js
@@ -0,0 +1,25 @@
+//Sublet App for the New TuftsLife
+
+function Listing() {
+	
+	//private function to initialize a new Listing
+	var init = function() {}
+	
+	//a unique id is attached to each listing, this is private
+	var id;
+	
+	this.lister;
+	this.address;
+	
+	this.description;
+	//images is an array of up to (5) images of the residence
+	this.images;
+	this.rent;
+	//contact holds a phone number, email address or other contact info
+	this.contact;
+	
+	//public function used to view a listing
+	this.view = function() {}
+	
+}
+
